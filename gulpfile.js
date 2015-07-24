be22e3324214c1b9f0f0b170337173c5027e7faf
Peter deHaan <peter@deseloper.com>
2015-07-24 13:05:30 -0700

    Generate the src/*.rdf files from a template

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
+const fs = require('fs');
+
 const del = require('del');
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
 const zip = require('gulp-zip');
-const fs = require('fs');
+const template = require('gulp-template');
 
-gulp.task('build', ['lint', 'clean:dist'], function () {
-  return gulp.src('src/**')
-    .pipe(zip('addon.xpi'))
-    .pipe(gulp.dest('dist'));
-});
+const pkgVersion = require('./package.json').version;
 
+// Delete everything in the `dist/*` directory.
 gulp.task('clean:dist', function (callback) {
   del(['dist'], callback);
 });
 
+// Copy the `src/*.rdf` files to `dist/*.rdf` for easier deployment.
+gulp.task('copy:rdf', ['generate:rdf'], function () {
+  return gulp.src('src/*.rdf')
+    .pipe(gulp.dest('dist'));
+});
+
+// Run ESLint against all the *.js files.
 gulp.task('eslint', function () {
   return gulp.src('{,src/**/,test/**/}*.js')
     .pipe(eslint())
@@ -33,3 +39,23 @@ gulp.task('gen-prefs', function(cb){
   contents += '//user_pref("services.universalSearch.baseURL", "https://d1fnkpeapwua2i.cloudfront.net/");\n';
   fs.writeFile('users.js', contents, cb);
 });
+
+// Generate the `src/install.rdf` and `src/update.rdf` files from the templates.
+gulp.task('generate:rdf', function () {
+  return gulp.src('templates/*.rdf')
+    .pipe(template({version: pkgVersion}))
+    .pipe(gulp.dest('src'));
+});
+
+// Generate the `dist/addon.xpi` file from the files in `src/**`.
+gulp.task('generate:xpi', function () {
+  return gulp.src('src/**')
+    .pipe(zip('addon.xpi'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['lint', 'clean:dist', 'copy:rdf', 'generate:xpi']);
+
+gulp.task('lint', ['eslint']);
+
+gulp.task('default', ['build']);
