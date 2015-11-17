89b29c4ca9396a45bc0a68e6415f3b3ee9b7e366
Peter deHaan <peter@deseloper.com>
2015-11-16 16:26:23 -0800

    Update eslint-config-universal-search version

--- gulpfile.js
+++ gulpfile.js
@@ -9,24 +9,24 @@ const zip = require('gulp-zip');
 const pkgVersion = require('./package.json').version;
 
 // Delete everything in the `dist/*` directory.
-gulp.task('clean:dist', function (callback) {
+gulp.task('clean:dist', function(callback) {
   del(['dist'], callback);
 });
 
 // Copy the `src/*.rdf` files to `dist/*.rdf` for easier deployment.
-gulp.task('copy:rdf', ['generate:rdf'], function () {
+gulp.task('copy:rdf', ['generate:rdf'], function() {
   return gulp.src('src/*.rdf')
     .pipe(gulp.dest('dist'));
 });
 
 // Run ESLint against all the *.js files.
-gulp.task('eslint', function () {
+gulp.task('eslint', function() {
   return gulp.src('{,src/**/,test/**/}*.js')
     .pipe(eslint())
     .pipe(eslint.failOnError());
 });
 
-gulp.task('gen-prefs', function(cb){
+gulp.task('gen-prefs', function(cb) {
   var contents = '// Set prefs to use a local content server\n'; // eslint-disable-line
   contents += 'user_pref("services.universalSearch.frameURL", "https://localhost:8080/index.html");\n';
   contents += 'user_pref("services.universalSearch.baseURL", "https://localhost:8080/");\n';
@@ -37,14 +37,14 @@ gulp.task('gen-prefs', function(cb){
 });
 
 // Generate the `src/install.rdf` and `src/update.rdf` files from the templates.
-gulp.task('generate:rdf', function () {
+gulp.task('generate:rdf', function() {
   return gulp.src('templates/*.rdf')
     .pipe(template({version: pkgVersion}))
     .pipe(gulp.dest('src'));
 });
 
 // Generate the `dist/addon.xpi` file from the files in `src/**`.
-gulp.task('generate:xpi', function () {
+gulp.task('generate:xpi', function() {
   return gulp.src('src/**')
     .pipe(zip('addon.xpi'))
     .pipe(gulp.dest('dist'));
